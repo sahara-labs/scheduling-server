c71738ab5cf429c5946a85e66e450f665ecf2e55
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-02-09 21:55:29 +0000

    Still pretty broken, but in prinicple works.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@286 7ee224fa-57ca-4419-87f9-1f4314938d77

--- LocalRigProvider/src/au/edu/uts/eng/remotelabs/schedserver/rigprovider/LocalRigProviderActivator.java
+++ LocalRigProvider/src/au/edu/uts/eng/remotelabs/schedserver/rigprovider/LocalRigProviderActivator.java
@@ -3,51 +3,73 @@
  * Schedules and assigns local laboratory rigs.
  * 
  * @license See LICENSE in the top level directory for complete license terms.
- *          Copyright (c) 2010, University of Technology, Sydney
- *          All rights reserved.
- *          Redistribution and use in source and binary forms, with or without
- *          modification, are permitted provided that the following conditions are met:
- *          * Redistributions of source code must retain the above copyright notice,
- *          this list of conditions and the following disclaimer.
- *          * Redistributions in binary form must reproduce the above copyright
- *          notice, this list of conditions and the following disclaimer in the
- *          documentation and/or other materials provided with the distribution.
- *          * Neither the name of the University of Technology, Sydney nor the names
- *          of its contributors may be used to endorse or promote products derived from
- *          this software without specific prior written permission.
- *          THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *          AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *          IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *          DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *          FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *          DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *          SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *          CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *          OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *          OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * Copyright (c) 2010, University of Technology, Sydney
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  * Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the University of Technology, Sydney nor the names
+ *    of its contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *    
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
  * @author Michael Diponio (mdiponio)
  * @date 12th January 2010
  */
 package au.edu.uts.eng.remotelabs.schedserver.rigprovider;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServlet;
+
+import org.apache.axis2.transport.http.AxisServlet;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+import au.edu.uts.eng.remotelabs.schedserver.logger.Logger;
+import au.edu.uts.eng.remotelabs.schedserver.logger.LoggerActivator;
+import au.edu.uts.eng.remotelabs.schedserver.server.ServletServerService;
 
 /**
  * Activator for the local rig provider bundle.
  */
 public class LocalRigProviderActivator implements BundleActivator
 {
-
     @Override
-    public void start(final BundleContext arg0) throws Exception
+    public void start(final BundleContext context) throws Exception
     {
-        // TODO Auto-generated method stub
+        Logger logger = LoggerActivator.getLogger();
+        logger.priority("Starting " + context.getBundle().getSymbolicName());
+        
+        HttpServlet servlet = new AxisServlet();
+        ServletServerService service = new ServletServerService(servlet, "/lrp/*", 
+                true, new File("./repo/").getCanonicalPath());
+        ServiceRegistration reg = context.registerService(ServletServerService.class.getName(), service, null);
+        
+        
 
     }
 
     @Override
-    public void stop(final BundleContext arg0) throws Exception
+    public void stop(final BundleContext context) throws Exception
     {
         // TODO Auto-generated method stub
 
