dfabf88a6188b5c5677aaf345484c2e2088a1df4
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-02-18 03:35:57 +0000

    Completed adding a local rig.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@332 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ LocalRigProvider/src/au/edu/uts/eng/remotelabs/schedserver/rigprovider/impl/UpdateLocalRigStatus.java
@@ -0,0 +1,135 @@
+/**
+ * SAHARA Scheduling Server
+ *
+ * Schedules and assigns local laboratory rigs.
+ *
+ * @license See LICENSE in the top level directory for complete license terms.
+ *
+ * Copyright (c) 2010, University of Technology, Sydney
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the University of Technology, Sydney nor the names 
+ *    of its contributors may be used to endorse or promote products derived from 
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Michael Diponio (mdiponio)
+ * @date 4th January 2010
+ */
+package au.edu.uts.eng.remotelabs.schedserver.rigprovider.impl;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.dao.RigDao;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.Rig;
+import au.edu.uts.eng.remotelabs.schedserver.logger.Logger;
+import au.edu.uts.eng.remotelabs.schedserver.logger.LoggerActivator;
+
+/**
+ * Updates the status of an exist rig. The rig status is composed of a
+ * status flag and optionally, if the status is bad (false), the reason
+ * is it is bad.
+ */
+public class UpdateLocalRigStatus
+{
+    /** Rig data access object. */
+    private final RigDao rigDao;
+    
+    /** Reason updating a rigs status failed. */
+    private String errorReason;
+    
+    /** Logger. */
+    private final Logger logger;
+    
+    
+    public UpdateLocalRigStatus()
+    {
+        this.logger = LoggerActivator.getLogger();
+        this.rigDao = new RigDao();
+    }
+    
+    public UpdateLocalRigStatus(Session ses)
+    {
+        this.logger = LoggerActivator.getLogger();
+        this.rigDao = new RigDao(ses);
+    }
+    
+    /**
+     * Updates the rig with the provided name status. If the rig is inactive,
+     * it is reactivated.
+     * <br />
+     * Activating an inactive rig, using a status update is questionable and 
+     * may lead to active uncommunicable rigs, but is left to deal with
+     * possible sloppy rig clients.
+     * 
+     * @param name the rigs name
+     * @param online true if the rig is online
+     * @param offlineReason reason the rig is offline, null if the rig is \ 
+     *         online
+     * @return true if successfully updated the rigs status, false otherwise
+     */
+    public boolean updateStatus(String name, boolean online, String offlineReason)
+    {
+        this.logger.debug("Updating the status of '" + name + "', setting it to " + (online ? "online" : 
+                "offline with reason " + offlineReason) + '.');
+        
+        Rig rig = this.rigDao.findByName(name);
+        if (rig == null)
+        {
+            this.errorReason = "Rig " + name + " does not exist.";
+            this.logger.warn("Unable to the status of rig with name '" + name + "' as it does not exist.");
+            return false;
+        }
+        
+        if (!rig.isActive())
+        {
+            rig.setActive(true);
+            this.logger.info("Reactivating rig with name '" + name + "' because a status update was received.");
+        }
+        
+        rig.setOnline(online);
+        rig.setOfflineReason(offlineReason);
+        rig.setLastUpdateTimestamp(new Date());
+        
+        this.rigDao.flush();
+        return true;
+    }
+
+    /**
+     * @return the errorReason
+     */
+    public String getErrorReason()
+    {
+        return this.errorReason;
+    }
+    
+    /**
+     * Returns the in use database session.
+     * 
+     * @return database session
+     */
+    public Session getSession()
+    {
+        return this.getSession();
+    }
+}
---
+++ LocalRigProvider/src/au/edu/uts/eng/remotelabs/schedserver/rigprovider/impl/tests/UpdateLocalRigStatusTester.java
@@ -0,0 +1,306 @@
+/**
+ * SAHARA Scheduling Server
+ *
+ * Schedules and assigns local laboratory rigs.
+ *
+ * @license See LICENSE in the top level directory for complete license terms.
+ *
+ * Copyright (c) 2010, University of Technology, Sydney
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the University of Technology, Sydney nor the names 
+ *    of its contributors may be used to endorse or promote products derived from 
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Michael Diponio (mdiponio)
+ * @date 4th January 2010
+ */
+package au.edu.uts.eng.remotelabs.schedserver.rigprovider.impl.tests;
+
+
+import java.lang.reflect.Field;
+import java.util.Date;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.junit.Before;
+import org.junit.Test;
+
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.DataAccessActivator;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.dao.RigCapabilitiesDao;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.dao.RigDao;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.dao.RigTypeDao;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.AcademicPermission;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.Config;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.MatchingCapabilities;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.MatchingCapabilitiesId;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.RequestCapabilities;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.ResourcePermission;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.Rig;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.RigCapabilities;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.RigType;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.User;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.UserAssociation;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.UserAssociationId;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.UserClass;
+import au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.UserLock;
+import au.edu.uts.eng.remotelabs.schedserver.logger.LoggerActivator;
+import au.edu.uts.eng.remotelabs.schedserver.logger.impl.SystemErrLogger;
+import au.edu.uts.eng.remotelabs.schedserver.rigprovider.impl.UpdateLocalRigStatus;
+
+/**
+ * Tests the {@link UpdateLocalRigStatus} class.
+ */
+public class UpdateLocalRigStatusTester extends TestCase
+{
+    /** Object of class under test. */
+    private UpdateLocalRigStatus update;
+    
+    public UpdateLocalRigStatusTester(String name) throws Exception
+    {
+        super(name);
+        
+        /* Set up the logger. */
+        Field f = LoggerActivator.class.getDeclaredField("logger");
+        f.setAccessible(true);
+        f.set(null, new SystemErrLogger());
+        
+        /* Set up the SessionFactory. */
+        AnnotationConfiguration cfg = new AnnotationConfiguration();
+        Properties props = new Properties();
+        props.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+        props.setProperty("hibernate.connection.url", "jdbc:mysql://127.0.0.1:3306/sahara");
+        props.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLInnoDBDialect");
+        props.setProperty("hibernate.connection.username", "sahara");
+        props.setProperty("hibernate.connection.password", "saharapasswd");
+        props.setProperty("hibernate.show_sql", "true");
+        props.setProperty("hibernate.format_sql", "true");
+        props.setProperty("hibernate.use_sql_comments", "true");
+        props.setProperty("hibernate.generate_statistics", "true");
+        cfg.setProperties(props);
+        cfg.addAnnotatedClass(AcademicPermission.class);
+        cfg.addAnnotatedClass(Config.class);
+        cfg.addAnnotatedClass(MatchingCapabilities.class);
+        cfg.addAnnotatedClass(MatchingCapabilitiesId.class);
+        cfg.addAnnotatedClass(RequestCapabilities.class);
+        cfg.addAnnotatedClass(ResourcePermission.class);
+        cfg.addAnnotatedClass(Rig.class);
+        cfg.addAnnotatedClass(RigCapabilities.class);
+        cfg.addAnnotatedClass(RigType.class);
+        cfg.addAnnotatedClass(au.edu.uts.eng.remotelabs.schedserver.dataaccess.entities.Session.class);
+        cfg.addAnnotatedClass(User.class);
+        cfg.addAnnotatedClass(UserAssociation.class);
+        cfg.addAnnotatedClass(UserAssociationId.class);
+        cfg.addAnnotatedClass(UserClass.class);
+        cfg.addAnnotatedClass(UserLock.class);
+        
+        f = DataAccessActivator.class.getDeclaredField("sessionFactory");
+        f.setAccessible(true);
+        f.set(null, cfg.buildSessionFactory());
+    }
+    
+    @Override
+    @Before
+    public void setUp() throws Exception
+    {
+        this.update = new UpdateLocalRigStatus();
+    }
+    
+    @Test
+    public void testUpdateStatus()
+    {
+        Session ses = DataAccessActivator.getNewSession();
+        RigType type = new RigType("ulrsTypr", 180, false);
+        new RigTypeDao(ses).persist(type);
+        RigCapabilities caps = new RigCapabilities("ulrsA,urlsB");
+        new RigCapabilitiesDao(ses).persist(caps);
+        Rig rig = new Rig(type, caps, "urls", "http://urls", new Date(System.currentTimeMillis() - 86400001), // A day ago
+                true, null, false, false, true);
+        new RigDao(ses).persist(rig);
+        
+        assertTrue(this.update.updateStatus("urls", true, null));
+        
+        ses.refresh(rig);
+        assertTrue(rig.isActive());
+        assertTrue(rig.isOnline());
+        assertNull(rig.getOfflineReason());
+        
+        Date ts = rig.getLastUpdateTimestamp();
+        assertNotNull(ts);
+        assertTrue(new Date().after(ts));
+        assertTrue(new Date(System.currentTimeMillis() - 5000).before(ts));
+        
+        ses.beginTransaction();
+        ses.delete(rig);
+        ses.delete(type);
+        ses.delete(caps);
+        ses.getTransaction().commit();
+    }
+    
+    @Test
+    public void testUpdateStatusGoodToBad()
+    {
+        Session ses = DataAccessActivator.getNewSession();
+        RigType type = new RigType("ulrsTypr", 180, false);
+        new RigTypeDao(ses).persist(type);
+        RigCapabilities caps = new RigCapabilities("ulrsA,urlsB");
+        new RigCapabilitiesDao(ses).persist(caps);
+        Rig rig = new Rig(type, caps, "urls", "http://urls", new Date(System.currentTimeMillis() - 86400001), // A day ago
+                true, null, false, false, true);
+        
+        /* Good. */
+        rig.setOnline(true);
+        rig.setOfflineReason(null);
+        new RigDao(ses).persist(rig);
+        
+        assertTrue(this.update.updateStatus("urls", false, "Broken."));
+        
+        ses.refresh(rig);
+        assertTrue(rig.isActive());
+        assertFalse(rig.isOnline());
+        assertNotNull(rig.getOfflineReason());
+        assertEquals("Broken.", rig.getOfflineReason());
+        
+        Date ts = rig.getLastUpdateTimestamp();
+        assertNotNull(ts);
+        assertTrue(new Date().after(ts));
+        assertTrue(new Date(System.currentTimeMillis() - 5000).before(ts));
+        
+        ses.beginTransaction();
+        ses.delete(rig);
+        ses.delete(type);
+        ses.delete(caps);
+        ses.getTransaction().commit();
+    }
+    
+    @Test
+    public void testUpdateStatusBadToGood()
+    {
+        Session ses = DataAccessActivator.getNewSession();
+        RigType type = new RigType("ulrsTypr", 180, false);
+        new RigTypeDao(ses).persist(type);
+        RigCapabilities caps = new RigCapabilities("ulrsA,urlsB");
+        new RigCapabilitiesDao(ses).persist(caps);
+        Rig rig = new Rig(type, caps, "urls", "http://urls", new Date(System.currentTimeMillis() - 86400001), // A day ago
+                true, null, false, false, true);
+        
+        /* Bad. */
+        rig.setOnline(false);
+        rig.setOfflineReason("Broken");
+        new RigDao(ses).persist(rig);
+        
+        assertTrue(this.update.updateStatus("urls", true, null));
+        
+        ses.refresh(rig);
+        assertTrue(rig.isActive());
+        assertTrue(rig.isOnline());
+        assertNull(rig.getOfflineReason());
+        
+        Date ts = rig.getLastUpdateTimestamp();
+        assertNotNull(ts);
+        assertTrue(new Date().after(ts));
+        assertTrue(new Date(System.currentTimeMillis() - 5000).before(ts));
+        
+        ses.beginTransaction();
+        ses.delete(rig);
+        ses.delete(type);
+        ses.delete(caps);
+        ses.getTransaction().commit();
+    }
+    
+    @Test
+    public void testUpdateStatusGoodToBadInActive()
+    {
+        Session ses = DataAccessActivator.getNewSession();
+        RigType type = new RigType("ulrsTypr", 180, false);
+        new RigTypeDao(ses).persist(type);
+        RigCapabilities caps = new RigCapabilities("ulrsA,urlsB");
+        new RigCapabilitiesDao(ses).persist(caps);
+        Rig rig = new Rig(type, caps, "urls", "http://urls", new Date(System.currentTimeMillis() - 86400001), // A day ago
+                true, null, false, false, true);
+        
+        /* Good. */
+        rig.setOnline(true);
+        rig.setOfflineReason(null);
+        rig.setActive(false);
+        new RigDao(ses).persist(rig);
+        
+        assertTrue(this.update.updateStatus("urls", false, "Broken."));
+        
+        ses.refresh(rig);
+        assertTrue(rig.isActive());
+        assertFalse(rig.isOnline());
+        assertNotNull(rig.getOfflineReason());
+        assertEquals("Broken.", rig.getOfflineReason());
+        
+        Date ts = rig.getLastUpdateTimestamp();
+        assertNotNull(ts);
+        assertTrue(new Date().after(ts));
+        assertTrue(new Date(System.currentTimeMillis() - 5000).before(ts));
+        
+        ses.beginTransaction();
+        ses.delete(rig);
+        ses.delete(type);
+        ses.delete(caps);
+        ses.getTransaction().commit();
+    }
+    
+    @Test
+    public void testUpdateStatusBadToGoodInActive()
+    {
+        Session ses = DataAccessActivator.getNewSession();
+        RigType type = new RigType("ulrsTypr", 180, false);
+        new RigTypeDao(ses).persist(type);
+        RigCapabilities caps = new RigCapabilities("ulrsA,urlsB");
+        new RigCapabilitiesDao(ses).persist(caps);
+        Rig rig = new Rig(type, caps, "urls", "http://urls", new Date(System.currentTimeMillis() - 86400001), // A day ago
+                true, null, false, false, true);
+        
+        /* Bad. */
+        rig.setOnline(false);
+        rig.setOfflineReason("Broken");
+        rig.setActive(false);
+        new RigDao(ses).persist(rig);
+        
+        assertTrue(this.update.updateStatus("urls", true, null));
+        
+        ses.refresh(rig);
+        assertTrue(rig.isActive());
+        assertTrue(rig.isOnline());
+        assertNull(rig.getOfflineReason());
+        
+        Date ts = rig.getLastUpdateTimestamp();
+        assertNotNull(ts);
+        assertTrue(new Date().after(ts));
+        assertTrue(new Date(System.currentTimeMillis() - 5000).before(ts));
+        
+        ses.beginTransaction();
+        ses.delete(rig);
+        ses.delete(type);
+        ses.delete(caps);
+        ses.getTransaction().commit();
+    }
+
+}
