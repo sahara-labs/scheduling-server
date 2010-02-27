aa892f3085d04e70533fe4a069728a0902a39fb8
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-02-27 05:45:38 +0000

    Added the Permissions project.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@375 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ Permissions/src/au/edu/uts/eng/remotelabs/schedserver/permissions/PermissionActivator.java
@@ -0,0 +1,22 @@
+package au.edu.uts.eng.remotelabs.schedserver.permissions;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class PermissionActivator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
