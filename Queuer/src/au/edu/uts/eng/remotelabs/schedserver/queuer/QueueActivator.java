53e5254e5aebb0b434b17df2c5c8247649489fe1
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-03-28 00:48:49 +0000

    Started queuer project
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@544 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ Queuer/src/au/edu/uts/eng/remotelabs/schedserver/queuer/QueueActivator.java
@@ -0,0 +1,22 @@
+package au.edu.uts.eng.remotelabs.schedserver.queuer;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class QueueActivator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
