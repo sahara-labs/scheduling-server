0a45ce4edd7505621d2d7cd82e397dad62479b74
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-05-13 07:57:15 +0000

    Files for the LabConnector, which will be a SchedServer bundle. Basic skeleton. Need to add the following: 1. Relevant comments 2. Ant task 3. Implement activator 4. Register the OSGi bundle with rest of SchedServer package 5. Web Service Client w/ possibly SOAP Header replacement:    a.) For .NET LabConnector WebService    b.) For ESS WebService
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@851 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ LabConnector/src/au/edu/labshare/schedserver/labconnector/LabConnectorActivator.java
@@ -0,0 +1,22 @@
+package au.edu.labshare.schedserver.labconnector;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class LabConnectorActivator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
