18091865341bef749cc19ffdfbb86e23bfb8545f
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-08-11 03:50:17 +0000

    Adding Axis2 Services Code that was generated. This has only been partially cleaned up. Needs more changes to be done. Will do for time being.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@1456 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ ScormPackager/src/au/edu/labshare/schedserver/scormpackager/service/ScormPackagerSkeletonInterface.java
@@ -0,0 +1,114 @@
+
+/**
+ * ScormPackagerSkeletonInterface.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.4  Built on : Apr 26, 2008 (06:24:30 EDT)
+ */
+    package au.edu.labshare.schedserver.scormpackager.service;
+    
+    /**
+     *  ScormPackagerSkeletonInterface java skeleton interface for the axisService
+     */
+    public interface ScormPackagerSkeletonInterface {
+     
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param deleteSCO
+         */
+
+        
+                public au.edu.labshare.schedserver.scormpackager.types.DeleteSCOResponse deleteSCO
+                (
+                  au.edu.labshare.schedserver.scormpackager.types.DeleteSCO deleteSCO
+                 )
+            ;
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param validateSCO
+         */
+
+        
+                public au.edu.labshare.schedserver.scormpackager.types.ValidateSCOResponse validateSCO
+                (
+                  au.edu.labshare.schedserver.scormpackager.types.ValidateSCO validateSCO
+                 )
+            ;
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param createPIF
+         */
+
+        
+                public au.edu.labshare.schedserver.scormpackager.types.CreatePIFResponse createPIF
+                (
+                  au.edu.labshare.schedserver.scormpackager.types.CreatePIF createPIF
+                 )
+            ;
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param createSCO
+         */
+
+        
+                public au.edu.labshare.schedserver.scormpackager.types.CreateSCOResponse createSCO
+                (
+                  au.edu.labshare.schedserver.scormpackager.types.CreateSCO createSCO
+                 )
+            ;
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param validatePIF
+         */
+
+        
+                public au.edu.labshare.schedserver.scormpackager.types.ValidatePIFResponse validatePIF
+                (
+                  au.edu.labshare.schedserver.scormpackager.types.ValidatePIF validatePIF
+                 )
+            ;
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param deletePIF
+         */
+
+        
+                public au.edu.labshare.schedserver.scormpackager.types.DeletePIFResponse deletePIF
+                (
+                  au.edu.labshare.schedserver.scormpackager.types.DeletePIF deletePIF
+                 )
+            ;
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                    * @param validateManifest
+         */
+
+        
+                public au.edu.labshare.schedserver.scormpackager.types.ValidateManifestResponse validateManifest
+                (
+                  au.edu.labshare.schedserver.scormpackager.types.ValidateManifest validateManifest
+                 )
+            ;
+        
+         }
+    
