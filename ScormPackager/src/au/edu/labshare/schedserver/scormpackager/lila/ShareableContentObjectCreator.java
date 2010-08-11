8ff83472cca8590b2dcda33c98cc7bf0315f3710
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-08-11 08:27:16 +0000

    1. Adding webservice calling file to implement the Skeleton interface. 2. Some manifest logic to create the manifest code (still needs to be changed).
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@1461 7ee224fa-57ca-4419-87f9-1f4314938d77

--- ScormPackager/src/au/edu/labshare/schedserver/scormpackager/lila/ShareableContentObjectCreator.java
+++ ScormPackager/src/au/edu/labshare/schedserver/scormpackager/lila/ShareableContentObjectCreator.java
@@ -12,18 +12,33 @@ import java.util.zip.Deflater;
 import java.util.Collection;
 import java.util.Iterator;
 
+//Needed for XML generation
+import javax.imageio.IIOException;
+import javax.xml.stream.XMLEventFactory;
+import javax.xml.stream.XMLEventWriter;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.EndElement;
+import javax.xml.stream.events.StartDocument;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
 import au.edu.labshare.schedserver.scormpackager.lila.ShareableContentObjectCreatorAdaptor;
 
 public class ShareableContentObjectCreator extends ShareableContentObjectCreatorAdaptor  
 {
+	public static final String MANFEST_NAME = "imsmanfest.xml";
+	
 	private static final String SCO_FILENAME = null;
-	private static final String GENERIC_TITLE = "SCORM";
-	private static final String SCO_FILEPATH = "/SCOs/";
+	private static final String GENERIC_TITLE = "Generic Title";
+	private static final String SCO_FILEPATH = "/SCOs";
 	private static final int BUFFER_SIZE = 18024;
 	
 	@Override
 	public String createSCO(String title, Collection <File> assets, String LMSName) 
 	{
+		String zipFileName = null;
 		String filePathSCO = null;
 		String filePathManifest = null;
 		FileInputStream fileInStream = null;
@@ -37,6 +52,9 @@ public class ShareableContentObjectCreator extends ShareableContentObjectCreator
 		
 		// TODO Need to do the autogeneration of the manifest file.
 		filePathManifest = generateManifest(title, assets);
+			
+		// TODO Need to process the title and replace the name with underscores
+		zipFileName  = removeWhiteSpace(title);
 		
 		//Zip the contents into a file
 		try 
@@ -83,13 +101,17 @@ public class ShareableContentObjectCreator extends ShareableContentObjectCreator
 			    fileInStream.close();
 			    
 			    
-			    filePathSCO = SCO_FILEPATH + title + ".zip";
+			    filePathSCO = SCO_FILEPATH + "/" + zipFileName + ".zip";
 				zipFileOutStream.close();
 				
 				//TODO Need to associate the ZipFile object type to the zipFileOutStream/ZipEntries.
 				return filePathSCO;
 			}
 			catch(IOException e)
+			{
+				e.printStackTrace(); //TODO replace with Sahara Logger as part of refactoring.
+			}
+			catch(NullPointerException e)
 			{
 				e.printStackTrace(); //TODO replace with Sahara Logger as part of refactoring.
 			}
@@ -150,18 +172,107 @@ public class ShareableContentObjectCreator extends ShareableContentObjectCreator
 	}
 
 	@Override
-	public boolean validateContent(ZipFile PIF) {
+	public boolean validateContent(ZipFile PIF) 
+	{
 		// TODO Auto-generated method stub
 		return false;
 	}
 	
 	/**
 	 * Generates the lmsmanifest.xml file. This is not going to be public
+	 * 
+	 * Based on: http://www.vogella.de/articles/JavaXML/article.html
 	 * @param title
 	 * @param assets
 	 */
-	private String generateManifest(String title, Collection<File> assets) {
+	private String generateManifest(String title, Collection<File> assets) 
+	{
+		//Create the XML file 
+		try
+		{
+			// Create a XMLOutputFactory
+			XMLOutputFactory outputFactory = XMLOutputFactory.newInstance();
+			// Create XMLEventWriter
+			XMLEventWriter eventWriter = outputFactory
+					.createXMLEventWriter(new FileOutputStream(MANFEST_NAME));
+			// Create a EventFactory
+			XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+			XMLEvent end = eventFactory.createDTD("\n");
+			// Create and write Start Tag
+			StartDocument startDocument = eventFactory.createStartDocument();
+			eventWriter.add(startDocument);
+
+			// Create config open tag
+			StartElement configStartElement = eventFactory.createStartElement("",
+					"", "config");
+			eventWriter.add(configStartElement);
+			eventWriter.add(end);
+			// Write the different nodes
+			createNode(eventWriter, "mode", "1");
+			createNode(eventWriter, "unit", "901");
+			createNode(eventWriter, "current", "0");
+			createNode(eventWriter, "interactive", "0");
+
+			eventWriter.add(eventFactory.createEndElement("", "", "config"));
+			eventWriter.add(end);
+			eventWriter.add(eventFactory.createEndDocument());
+			eventWriter.close();
+			
+		}
+		catch (XMLStreamException e) 
+		{
+			e.printStackTrace();//TODO replace with Sahara Logger as part of refactoring.
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();//TODO replace with Sahara Logger as part of refactoring.
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Takes the value of a name to produce an underscore version of the name.
+	 * This is to allow naming consistency.
+	 * 
+	 * This is from: http://www.vogella.de/articles/JavaXML/article.html
+	 * @param name
+	 */
+	private void createNode(XMLEventWriter eventWriter, String name, String value)
+	{
+		try
+		{
+			XMLEventFactory eventFactory = XMLEventFactory.newInstance();
+			XMLEvent end = eventFactory.createDTD("\n");
+			XMLEvent tab = eventFactory.createDTD("\t");
+			// Create Start node
+			StartElement sElement = eventFactory.createStartElement("", "", name);
+			eventWriter.add(tab);
+			eventWriter.add(sElement);
+			// Create Content
+			Characters characters = eventFactory.createCharacters(value);
+			eventWriter.add(characters);
+			// Create End node
+			EndElement eElement = eventFactory.createEndElement("", "", name);
+			eventWriter.add(eElement);
+			eventWriter.add(end);
+		}
+		catch(XMLStreamException e)
+		{
+			e.printStackTrace(); //TODO replace with Sahara Logger as part of refactoring.
+		}
+
+	}
+	
+	/**
+	 * Takes the value of a name to produce an underscore version of the name.
+	 * This is to allow naming consistency.
+	 * @param name
+	 */
+	private String removeWhiteSpace(String name) 
+	{
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 }
---
+++ ScormPackager/src/au/edu/labshare/schedserver/scormpackager/service/ScormPackager.java
@@ -0,0 +1,70 @@
+package au.edu.labshare.schedserver.scormpackager.service;
+
+import au.edu.labshare.schedserver.scormpackager.types.CreatePIF;
+import au.edu.labshare.schedserver.scormpackager.types.CreatePIFResponse;
+import au.edu.labshare.schedserver.scormpackager.types.CreateSCO;
+import au.edu.labshare.schedserver.scormpackager.types.CreateSCOResponse;
+import au.edu.labshare.schedserver.scormpackager.types.DeletePIF;
+import au.edu.labshare.schedserver.scormpackager.types.DeletePIFResponse;
+import au.edu.labshare.schedserver.scormpackager.types.DeleteSCO;
+import au.edu.labshare.schedserver.scormpackager.types.DeleteSCOResponse;
+import au.edu.labshare.schedserver.scormpackager.types.ValidateManifest;
+import au.edu.labshare.schedserver.scormpackager.types.ValidateManifestResponse;
+import au.edu.labshare.schedserver.scormpackager.types.ValidatePIF;
+import au.edu.labshare.schedserver.scormpackager.types.ValidatePIFResponse;
+import au.edu.labshare.schedserver.scormpackager.types.ValidateSCO;
+import au.edu.labshare.schedserver.scormpackager.types.ValidateSCOResponse;
+
+public class ScormPackager implements ScormPackagerSkeletonInterface
+{
+
+	@Override
+	public CreatePIFResponse createPIF(CreatePIF createPIF) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CreateSCOResponse createSCO(CreateSCO createSCO) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DeletePIFResponse deletePIF(DeletePIF deletePIF) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public DeleteSCOResponse deleteSCO(DeleteSCO deleteSCO) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidateManifestResponse validateManifest(ValidateManifest validateManifest) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidatePIFResponse validatePIF(ValidatePIF validatePIF) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ValidateSCOResponse validateSCO(ValidateSCO validateSCO) 
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
