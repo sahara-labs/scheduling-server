1cf46860340f5966c9699bde702bfee8e25edfa0
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-09-25 05:33:24 +0000

    Small change to display RigSCO than rather ExperimentSCO (semantics).
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@1736 7ee224fa-57ca-4419-87f9-1f4314938d77

--- ScormPackager/src/au/edu/labshare/schedserver/scormpackager/lila/Manifest.java
+++ ScormPackager/src/au/edu/labshare/schedserver/scormpackager/lila/Manifest.java
@@ -14,9 +14,9 @@ import au.edu.labshare.schedserver.scormpackager.utilities.ScormUtilities;
 
 public class Manifest implements IManifest
 {
-	public static final String GENERIC_IDENTIFER = "ExperimentSCO";
-	public static final String SCHEMA_VERSION = "1.0";
-	
+	public static final String GENERIC_IDENTIFER = "RigSCO"; //TODO: Should be able to change this title.
+	public static final String SCHEMA_VERSION = "1.0";		
+ 	
 	public static final String ITEM_NAME = "item";
 	public static final String HTML_EXT = "html";
 	public static final String NAMESPACE = "http://labshare.edu.au";
