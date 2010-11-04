e9609970f9d3ff17deea576a385ac84d6824ed4e
heyeung <heyeung@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-11-04 09:12:15 +0000

    Added a header with licensing and author. Also, added a short description about class.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@2019 7ee224fa-57ca-4419-87f9-1f4314938d77

--- ScormPackager/src/au/edu/labshare/schedserver/scormpackager/sahara/LaunchButtonDecorator.java
+++ ScormPackager/src/au/edu/labshare/schedserver/scormpackager/sahara/LaunchButtonDecorator.java
@@ -1,3 +1,42 @@
+/**
+ * SAHARA Scheduling Server
+ *
+ * LaunchButtonDecorator Class. 
+ * Allows the adding of a launch button to the html template to allow
+ * the user to be redirected to the URL of the rig client with the necessary parameters.
+ * This allows ONLY one experiment (rig type) to be accessed. Part of the RigSCO concept.
+ *
+ * @license See LICENSE in the top level directory for complete license terms.
+ *
+ * Copyright (c) 2010, University of Technology, Sydney
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the University of Technology, Sydney nor the names 
+ *    of its contributors may be used to endorse or promote products derived from 
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Herber Yeung
+ * @date 4th November 2010
+ */
 package au.edu.labshare.schedserver.scormpackager.sahara;
 
 import biz.source_code.miniTemplator.MiniTemplator;
