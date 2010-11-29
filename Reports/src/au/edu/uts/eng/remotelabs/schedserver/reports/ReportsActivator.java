32f0421ab3a3db621b089baa9e97c77fb72432f7
tmachet <tmachet@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-11-29 04:17:52 +0000

    First commit of Reports wsdl interface
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@2108 7ee224fa-57ca-4419-87f9-1f4314938d77

--- Reports/src/au/edu/uts/eng/remotelabs/schedserver/reports/ReportsActivator.java
+++ Reports/src/au/edu/uts/eng/remotelabs/schedserver/reports/ReportsActivator.java
@@ -10,6 +10,7 @@ public class ReportsActivator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext context) throws Exception {
+	    System.out.println("Reports Activator start");
 	}
 
 	/*
@@ -17,6 +18,7 @@ public class ReportsActivator implements BundleActivator {
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	public void stop(BundleContext context) throws Exception {
+	    System.out.println("Reports Activator stop");
 	}
 
 }
