83e769aaf9c43b6a92c05541fffd802e561128a9
tmachet <tmachet@7ee224fa-57ca-4419-87f9-1f4314938d77>
2010-11-29 00:39:51 +0000

    Initial creation of reports section
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@2107 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ Reports/src/au/edu/uts/eng/remotelabs/schedserver/reports/ReportsActivator.java
@@ -0,0 +1,22 @@
+package au.edu.uts.eng.remotelabs.schedserver.reports;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ReportsActivator implements BundleActivator {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+	}
+
+}
