3a42663cc236f7690e572dddf731cd29f9d0645d
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2011-05-19 03:16:49 +0000

    Importing the multisite bundle project.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/branches/multisite@3061 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ MultiSite/src/au/edu/uts/eng/remotelabs/schedserver/multisite/MultiSiteActivator.java
@@ -0,0 +1,30 @@
+package au.edu.uts.eng.remotelabs.schedserver.multisite;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class MultiSiteActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		MultiSiteActivator.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		MultiSiteActivator.context = null;
+	}
+
+}
