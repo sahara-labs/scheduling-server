d084fb287449d72f2c73e77d6fae7b32f12c599e
mdiponio <mdiponio@7ee224fa-57ca-4419-87f9-1f4314938d77>
2012-10-31 07:56:47 +0000

    Added rigs page.
    git-svn-id: https://develop.eng.uts.edu.au/projects/personal/sahara/svn/SchedServer/trunk@4044 7ee224fa-57ca-4419-87f9-1f4314938d77

---
+++ RigProvider/src/au/edu/uts/eng/remotelabs/schedserver/rigprovider/pages/Rigs.java
@@ -0,0 +1,71 @@
+/**
+ * SAHARA Scheduling Server
+ *
+ * Schedules and assigns local laboratory rigs.
+ *
+ * @license See LICENSE in the top level directory for complete license terms.
+ *
+ * Copyright (c) 2012, University of Technology, Sydney
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *  * Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright 
+ *    notice, this list of conditions and the following disclaimer in the 
+ *    documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the University of Technology, Sydney nor the names 
+ *    of its contributors may be used to endorse or promote products derived from 
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE 
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * @author Michael Diponio (mdiponio)
+ * @date 30th January 2012
+ */
+package au.edu.uts.eng.remotelabs.schedserver.rigprovider.pages;
+
+import javax.servlet.http.HttpServletRequest;
+
+import au.edu.uts.eng.remotelabs.schedserver.server.HostedPage;
+
+/**
+ * Page to administer rig types.
+ */
+public class Rigs extends AbstractRigProviderPage 
+{
+
+	@Override
+	public void setupView(HttpServletRequest req) 
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected String getPageType() 
+	{
+		return "Rigs";
+	}
+	
+	/**
+	 * Provides details about hosting this page.
+	 * 
+	 * @return populated
+	 */
+	public static HostedPage getHostedPage()
+    {
+        return new HostedPage("Rigs", Rigs.class, "rigs", "Allows rigs to be administered.", true, true);
+    }
+}
